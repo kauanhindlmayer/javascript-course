87e802342e02de23b47e435c8d7352f816d1468a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-04-05 10:52:46 -0300

    Desenvolvendo exercícios da aula 027

---
+++ aula027/index.js
@@ -0,0 +1,23 @@
+/*
+Entre 0 - 11 -> Bom dia!
+Entre 12 - 17 -> Boa tarde!
+Entre 18 e 23 -> Boa noite!
+*/
+
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes 
+// Eu posso ter vários else ifs na checagem
+// Só posso ter um else na checagem
+
+
+const hora = 50;
+
+if (hora >= 0 && hora <= 12) {
+    console.log('Bom dia!');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde!');
+} else if (hora>= 18 && hora <= 23) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá!');
+}
