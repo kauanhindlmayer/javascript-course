3cba4d737c92f6089426c6d76ca82c8ba400a13e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-04-05 17:26:49 -0300

    Removendo o exemplo

--- aula029/assets/js/exemplo.js
+++
@@ -1,7 +0,0 @@
-// Capturar evento submit do formulário
-const form = documet.querySelector('.form');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log('Evento prevenido!');
-});
