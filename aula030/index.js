f8ee32f60caf2b89e5f51975601b68f5f57163aa
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-04-07 14:57:37 -0300

    Aula 030

---
+++ aula030/index.js
@@ -0,0 +1,12 @@
+// (condicao) ? 'Valor para verdadeiro' : 'Valor para falso'
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
+
+/*
+if (pontuacaoUsuario >= 1000) {
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal');
+}
+*/
