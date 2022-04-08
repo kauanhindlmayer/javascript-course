0770fb8745330cda3e239b25bc7825a73c9f784a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-04-08 11:50:33 -0300

    Exercício aula 031

--- aula030/index.js
+++ aula030/index.js
@@ -3,6 +3,9 @@ const pontuacaoUsuario = 1000;
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
 console.log(nivelUsuario);
 
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+
 /*
 if (pontuacaoUsuario >= 1000) {
     console.log('Usuário VIP');
---
+++ aula031/index.js
@@ -0,0 +1,35 @@
+// const data = new Date(0); // 01/01/1970 Timestamp unix ou época unix
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(0 + tresHoras + umDia);
+/*
+const data = new Date('2019-04-20 20:20:59');
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth());
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Minutos', data.getMinutes());
+console.log('Segundos', data.getSeconds());
+console.log('Milissegundos', data.getMilliseconds());
+console.log('Dia da semana', data.getDay());
+console.log(data.toString());
+console.log(Date.now());
+*/
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const sec = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${sec}`
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
