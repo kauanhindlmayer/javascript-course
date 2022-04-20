b760097f10224c14410396c8a2432c5314fd3d2f
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-04-20 11:52:10 -0300

    Aulas 034 e 035

--- aula033/assets/js/main.js
+++ aula033/assets/js/main.js
@@ -2,44 +2,44 @@ function getMesTexto (numeroMes) {
     let mes;
     switch (numeroMes) {
         case 0: 
-            mes = 'Janeiro';
+            mes = 'janeiro';
             return mes;
         case 1: 
-            mes = 'Fevereiro';
+            mes = 'fevereiro';
             return mes;
         case 2: 
-            mes = 'Março';
+            mes = 'março';
             return mes;
         case 3: 
-            mes = 'Abril';
+            mes = 'abril';
             return mes;
         case 4: 
-            mes = 'Maio';
+            mes = 'maio';
             return mes;
         case 5: 
-            mes = 'Junho';
+            mes = 'junho';
             return mes; 
         case 6: 
-            mes = 'Julho';
+            mes = 'julho';
             return mes;  
         case 7: 
-            mes = 'Agosto';
+            mes = 'agosto';
             return mes;  
         case 8: 
-            mes = 'Setembro';
+            mes = 'setembro';
             return mes;  
         case 9: 
-            mes = 'Outubro';
+            mes = 'outubro';
             return mes;  
         case 10: 
-            mes = 'Novembro';
+            mes = 'novembro';
             return mes;  
         case 11: 
-            mes = 'Dezembro';
+            mes = 'dezembro';
             return mes;  
         default:
             mes = '';
-            return diaSemanaTexto;
+            return mes;
         }
 }
 
@@ -48,30 +48,34 @@ function getDiaSemanaTexto (diaSemana) {
 
     switch (diaSemana) {
         case 0: 
-            diaSemanaTexto = 'Domingo';
+            diaSemanaTexto = 'domingo';
             return diaSemanaTexto;
         case 1: 
-            diaSemanaTexto = 'Segunda-feira';
+            diaSemanaTexto = 'segunda-feira';
             return diaSemanaTexto;
         case 2: 
-            diaSemanaTexto = 'Terça-feira';
+            diaSemanaTexto = 'terça-feira';
             return diaSemanaTexto;
         case 3: 
-            diaSemanaTexto = 'Quarta-feira';
+            diaSemanaTexto = 'quarta-feira';
             return diaSemanaTexto;
         case 4: 
-            diaSemanaTexto = 'Quinta-feira';
+            diaSemanaTexto = 'quinta-feira';
             return diaSemanaTexto;
         case 5: 
-            diaSemanaTexto = 'Sexta-feira';
+            diaSemanaTexto = 'sexta-feira';
             return diaSemanaTexto;
         case 6: 
-            diaSemanaTexto = 'Sábado';
+            diaSemanaTexto = 'sábado';
             return diaSemanaTexto;  
         default:
             diaSemanaTexto = '';
             return diaSemanaTexto;
         }
+}
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
 }
 
 function criaData(data) {
@@ -82,11 +86,14 @@ function criaData(data) {
     const ano = data.getFullYear();
     const hora = data.getHours();
     const min = data.getMinutes();
-    const sec = data.getSeconds();
     
     const res = document.getElementById('res');
-    res.innerHTML = `${nomeDia}, ${diaSemana} de ${nomeMes} de ${ano}. </br> ${hora}:${min}:${sec}`;
+    res.innerHTML = `${nomeDia}, ${diaSemana} de ${nomeMes} de ${ano} </br> ${zeroAEsquerda(hora)}:${zeroAEsquerda(min)}`;
 }
 
 const data = new Date();
 criaData(data);
+
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+// h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short'});
---
+++ aula035/index.js
@@ -0,0 +1,14 @@
+// let a = 'A'; // B
+// let b = 'B'; // C
+// let c = 'C'; // A
+
+// const letras = [b, c, a];
+// [a, b, c] = letras;
+
+// console.log(a, b, c);
+
+//               0  1  2  3  4  5  6  7  8
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+const [primeiroNumero, segundoNumero, ...resto] = numeros;
+console.log(primeiroNumero, segundoNumero);
+console.log(resto);
