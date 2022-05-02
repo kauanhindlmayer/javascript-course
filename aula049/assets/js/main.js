e9a9d5a4778c5dcf2eba146f8f4ce9495d7fa8a1
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-05-02 11:46:47 -0300

    Adicionado botão apagar no To-do List

--- aula049/assets/js/main.js
+++ aula049/assets/js/main.js
@@ -7,17 +7,44 @@ function criaLi() {
   return li;
 }
 
-inputTarefa.addEventListener('keypress', function() {
-  
+inputTarefa.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+  }
 })
+
+function limpaInput() {
+  inputTarefa.value = '';
+  inputTarefa.focus();
+}
+
+function criaBotaoApagar (li) {
+  li.innerText += ' ';
+  const BotaoApagar = document.createElement('button');
+  BotaoApagar.innerText = 'Apagar';
+  // BotaoApagar.classList.add('apagar');;
+  BotaoApagar.setAttribute('class', 'apagar');
+  BotaoApagar.setAttribute('title', 'Apagar essa tarefa');
+  li.appendChild(BotaoApagar);
+}
 
 function criaTarefa(textoInput) {
   const li = criaLi();
   li.innerHTML = textoInput;
   tarefas.appendChild(li);
+  limpaInput();
+  criaBotaoApagar(li);
 }
 
 btnTarefa.addEventListener('click', function() {
   if (!inputTarefa.value) return;
   criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+  if (el.classList.contains('apagar')) {
+    console.log('Apagar clicacado!');
+  }
 });
