70e139e7f51e10ec4ac0f3dafca20e52cea5e22f
kauanhindlmayer <kauanindlmayer07@gmail.com>
2022-05-03 08:21:05 -0300

    Aula050

--- modulo004/aula050/index.js
+++ modulo004/aula050/index.js
@@ -3,3 +3,29 @@ function falaOi() {
   console.log('Oie');
 }
 falaOi();
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+  console.log('Sou um dado.');
+}
+
+function executaFuncao(funcao) {
+  console.log('Vou executar sua funçãoa abaixo: ');
+  funcao();
+}
+executaFuncao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+  console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const objeto = {
+  falar() {
+    console.log('Estou falando...');
+  }
+};
+objeto.falar();
