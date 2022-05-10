cc1c51dddd827212b0b7dc97383c4b72a7ebd47c
kauanhindlmayer <kauanhindlmayer07@gmail.com>
2022-05-10 17:16:44 -0300

    Exercícios

--- modulo004/aula058/assets/js/main.js
+++ modulo004/aula058/assets/js/main.js
@@ -1,1 +1,25 @@
-d
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+
+    inicia() {
+      this.cliqueBotoes();
+    },
+
+    cliqueBotoes() {
+      // this -> calculadora
+      document.addEventListener('click', function(e) {
+        const el = e.target;
+
+        if(el.classList.contains('btn-num')) {
+          this.btnParaDisplay();
+        }
+      });
+    },
+
+    
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
