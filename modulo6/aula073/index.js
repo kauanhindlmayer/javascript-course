204edc70a725c18b822a008d5596ef757d6d526c
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-05-19 10:28:04 -0300

    Exercícios

--- modulo6/aula073/index.js
+++ modulo6/aula073/index.js
@@ -3,13 +3,38 @@ function Produto(nome, preco, estoque) {
   this.nome = nome;
   this.preco = preco;
   
+  let estoquePrivado = estoque;
   Object.defineProperty(this, 'estoque', {
     enumerable: true, // mostra a chave
-    value: estoque, // valor da chave
-    writable: false, // pode alterar o valor
     configurable: false, // configurável
+    get: function() {
+      return estoquePrivado;
+    },
+    set: function(valor) {
+      if (typeof valor !== 'number') {
+        throw new TypeError('[ERRO]');
+      }
+      estoquePrivado = valor;
+    }
   });
 }
 
-const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1);
+function criaProduto(nome) {
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      valor = valor.replace('coisa.', '');
+      nome = valor;
+    }
+  };
+}
+
+// const p1 = new Produto('Camiseta', 20, 3);
+// // console.log(p1);
+// p1.estoque = 'O valor que eu quero';
+// console.log(p1.estoque);
+const p2 = criaProduto('Camiseta');
+p2.nome = 'Qualquer coisa.';
+console.log(p2.nome);
