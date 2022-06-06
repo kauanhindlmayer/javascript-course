84a8ee6305ed514396ec710c3e4453623d089715
kauanhindlmayer <kauanhindlmayer07@gmail.com>
2022-06-06 16:11:35 -0300

    Exercícios

---
+++ modulo8/091_fetch_API/assets/js/main.js
@@ -0,0 +1,60 @@
+// const request = obj => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+  
+//     xhr.addEventListener('load', () => {
+//       if(xhr.status >= 200 && xhr.status < 300) {
+//         resolve(xhr.responseText);
+//       } else {
+//         reject(xhr.statusText);
+//       }
+//     });
+//   });
+// };
+  
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+  
+    if(response.status !== 200) throw new Error('ERRO 404');
+    
+    const html = await response.text();
+    carregaResultado(html)
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
+
+// fetch('page1.html')
+//   .then(resposta => {
+//     if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+//     return resposta.text();
+//   })
+//   .then(html => console.log(html))
+//   .catch(e => console.error(e));
+
+
+/*
+  console.log();   => mensagem comum no console
+  console.warn();  => mensagem de aviso no console
+  console.error(); => mensagem de erro no console
+*/
