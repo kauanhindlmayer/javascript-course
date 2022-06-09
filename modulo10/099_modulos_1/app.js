dc69ea2d542edd8bd60bec17a345e375f8f5d76d
kauanhindlmayer <kauanhindlmayer07@gmail.com>
2022-06-09 17:45:54 -0300

    Exercícios

---
+++ modulo10/099_modulos_1/app.js
@@ -0,0 +1,17 @@
+const nome = 'Kauan';
+const sobrenome = 'Hindlmayer';
+
+const falaNome = () => {
+  console.log(nome, sobrenome);
+};
+
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
+console.log(module.exports);
+
