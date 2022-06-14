f7ddbdf95f6cd30dc6dac00c0c1d36cd83ef8fb0
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-14 10:30:38 -0300

    Exercícios

---
+++ modulo10/104_express_introducao/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// CRUD => Create, Read, Update, Delete
+//         Post    Get   Put     Delete
+
+// http://meusite.com/ <- GET -> Entregue a página /
+// http://meusite.com/sobre <- GET -> Entregue a página /sobre
+// http://meusite.com/contato <- GET -> Entregue a página /contato
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário.');
+});
+
+//                   request, response
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato com a gente.');
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
