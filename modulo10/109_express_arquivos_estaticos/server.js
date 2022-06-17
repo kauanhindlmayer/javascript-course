e7c9411f5fdf4e76752fdcb83eb289b6188d7eb3
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-17 11:45:26 -0300

    Exercícios

--- modulo10/109_express_arquivos_estaticos/server.js
+++ modulo10/109_express_arquivos_estaticos/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const routes = express('./routes');
+const routes = require('./routes');
 const path = require('path');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./public'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
