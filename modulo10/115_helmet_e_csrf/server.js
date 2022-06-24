3efbb7c31bb517d60362a7d2c5491e3d453f7972
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-24 09:30:24 -0300

    Exercícios

--- modulo10/115_helmet_e_csrf/server.js
+++ modulo10/115_helmet_e_csrf/server.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, UseUnifiedTopology: true })
   .then(() => {
     console.log('Conectei à base de dados.');
     app.emit('Pronto');
   })
   .catch(e => console.log(e));
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const session = require('express-session');
@@ -41,8 +40,11 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('Pronto', () => {
--- modulo10/115_helmet_e_csrf/src/middlewares/middleware.js
+++ modulo10/115_helmet_e_csrf/src/middlewares/middleware.js
@@ -6,3 +6,14 @@ exports.middlewareGlobal = (req, res, next) => {
 exports.outroMiddleware = (req, res, next) => {
   next();
 };
+
+exports.checkCsrfError = (err, req, res, next) => {
+  if(err && err.code == 'EBADCSRFTOKEN') {
+    return res.render('404');
+  }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+}
