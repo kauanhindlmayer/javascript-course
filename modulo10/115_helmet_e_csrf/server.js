23315e43bbcb133825bd69d5674548996229370e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-24 10:17:13 -0300

    Exercícios

--- modulo10/115_helmet_e_csrf/server.js
+++ modulo10/115_helmet_e_csrf/server.js
@@ -16,6 +16,7 @@ const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/midd
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const session = require('express-session');
