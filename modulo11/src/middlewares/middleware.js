5909408946745e5392abfaf507b7cae7f1035d41
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-25 10:39:47 -0300

    Exercícios

--- modulo11/src/middlewares/middleware.js
+++ modulo11/src/middlewares/middleware.js
@@ -8,9 +8,11 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && err.code == 'EBADCSRFTOKEN') {
+  if(err) {
     return res.render('404');
   }
+
+  next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
