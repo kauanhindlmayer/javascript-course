d6675978e573357b8ed8c61daaad6654e6f2ef49
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-27 21:55:47 -0300

    Exercícios

--- modulo11/routes.js
+++ modulo11/routes.js
@@ -10,6 +10,8 @@ route.get('/', homeController.index);
 // Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+
 
 
 module.exports = route;
