46a2b13ee092b5670c8637058baad000b46e9526
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-06-30 11:50:05 -0300

    Exercícios

--- modulo11/src/models/contatoModel.js
+++ modulo11/src/models/contatoModel.js
@@ -17,12 +17,6 @@ function Contato(body) {
   this.contato = null;
 }
 
-Contato.buscaPorId = async function(id) {
-  if(typeof id !== 'string') return;
-  const user = await ContatoModel.findById(id);
-  return user;
-};
-
 Contato.prototype.register = async function() {
   this.valida();
 
@@ -40,7 +34,7 @@ Contato.prototype.valida = function() {
   if(!this.body.email && !this.body.telefone) {
     this.errors.push('Pelo menos um contato precisa ser enviado: e-mail ou telefone.');
   }
-}
+};
 
 Contato.prototype.cleanUp = function() {
   for(const key in this.body) {
@@ -56,6 +50,32 @@ Contato.prototype.cleanUp = function() {
     telefone: this.body.telefone,
 
   };
-}
+};
+
+Contato.prototype.edit = async function(id) {
+  if(typeof id !== 'string') return;
+  this.valida();
+  if(this.errors.length > 0) return;
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+};
+
+// Métodos estáticos
+Contato.buscaPorId = async function(id) {
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findById(id);
+  return contato;
+};
+
+Contato.buscaContatos = async function() {
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 });
+  return contatos;
+};
+
+Contato.delete = async function(id) {
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findOneAndDelete(id);
+  return contato;
+};
 
 module.exports = Contato;
