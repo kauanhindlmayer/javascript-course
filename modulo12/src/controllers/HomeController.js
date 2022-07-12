3f21bc0570d165b61eb50e6b2ccc4f6316359110
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-12 16:04:24 -0300

    Exercícios

--- modulo12/src/controllers/HomeController.js
+++ modulo12/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Kauan',
-      sobrenome: 'Hindlmayer',
-      email: 'kauanhindlmayer07@gmail.com',
-      idade: 17,
-      peso: 76.5,
-      altura: 1.82,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
