a6ab6ffb3128c7d661d598808a38a3721275679d
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-13 11:53:24 -0300

    Exercícios

--- modulo12/src/controllers/FotoController.js
+++ modulo12/src/controllers/FotoController.js
@@ -1,6 +1,25 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+import Foto from '../models/Foto';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
-  async store(req, res) {
-    res.json(req.file);
+  store(req, res) {
+    return upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      return res.json(foto);
+    });
   }
 }
 
--- modulo12/src/routes/fotoRoutes.js
+++ modulo12/src/routes/fotoRoutes.js
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import multer from 'multer';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
