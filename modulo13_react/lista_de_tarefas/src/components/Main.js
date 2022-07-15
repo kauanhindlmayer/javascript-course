be5e15824c1846311eec7c33440988a4884cd294
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-15 10:00:39 -0300

    Exercícios

--- modulo13_react/lista_de_tarefas/src/components/Main.js
+++ modulo13_react/lista_de_tarefas/src/components/Main.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 //Form
 import { FaPlus } from 'react-icons/fa';
 
+//Tarefas
+import { FaEdit, FaRegTrashAlt } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: ['Fazer café', 'Beber água', 'Estudar'],
   };
 
   handleChange = (e) => {
@@ -17,18 +21,30 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
         <form action="#" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa}/>
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaRegTrashAlt className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
