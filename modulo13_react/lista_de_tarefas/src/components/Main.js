ec174c368bbff75c50e6d05c918e82afb582ddc8
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-15 11:51:56 -0300

    Exercícios

--- modulo13_react/lista_de_tarefas/src/components/Main.js
+++ modulo13_react/lista_de_tarefas/src/components/Main.js
@@ -11,7 +11,22 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: ['Fazer café', 'Beber água', 'Estudar'],
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
   };
 
   handleChange = (e) => {
@@ -27,7 +42,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input onChange={this.handleChange} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
@@ -38,10 +53,10 @@ export default class Main extends Component {
           {tarefas.map((tarefa) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
+              <span>
                 <FaEdit className="edit" />
                 <FaRegTrashAlt className="delete" />
-              </div>
+              </span>
             </li>
           ))}
         </ul>
