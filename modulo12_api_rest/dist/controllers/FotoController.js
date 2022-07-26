a7ed866d96409916a24c1931105cec79269eb5ac
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-26 14:19:31 -0300

    Exercícios

---
+++ modulo12_api_rest/dist/controllers/FotoController.js
@@ -0,0 +1,32 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _multer = require('multer'); var _multer2 = _interopRequireDefault(_multer);
+var _multer3 = require('../config/multer'); var _multer4 = _interopRequireDefault(_multer3);
+
+var _Foto = require('../models/Foto'); var _Foto2 = _interopRequireDefault(_Foto);
+
+const upload = _multer2.default.call(void 0, _multer4.default).single('foto');
+
+class FotoController {
+  store(req, res) {
+    return upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await _Foto2.default.create({ originalname, filename, aluno_id });
+
+        return res.json(foto);
+      } catch (e) {
+        return res.json(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+    });
+  }
+}
+
+exports. default = new FotoController();
