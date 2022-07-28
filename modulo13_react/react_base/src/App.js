c997d3ccb044ef634aeef38642e73c70dfc6b455
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-28 11:47:27 -0300

    Exercícios

--- modulo13_react/react_base/src/App.js
+++ modulo13_react/react_base/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
     <>
-      <Login />
+      <Header />
+      <Routes />
       <GlobalStyles />
     </>
   );
---
+++ modulo13_react/react_base/src/pages/Page404/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Page404() {
+  return <h1>Essa página não existe!</h1>;
+}
