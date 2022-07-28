ca4bb2a569d8bb74596adf099e3197f09ab75c0d
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-28 10:42:31 -0300

    Exercícios

--- modulo13_react/react_base/src/App.js
+++ modulo13_react/react_base/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Login from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
