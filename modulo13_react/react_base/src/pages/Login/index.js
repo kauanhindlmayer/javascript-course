10b6242a27bb60dc4b44b9da4ee1c668d9f27d6a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-29 11:17:57 -0300

    Exercícios

--- modulo13_react/react_base/src/pages/Login/index.js
+++ modulo13_react/react_base/src/pages/Login/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
   return (
     <Container>
-      <Title>
-        Login
-        <small>Olá</small>
-      </Title>
+      <Title>Login</Title>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
