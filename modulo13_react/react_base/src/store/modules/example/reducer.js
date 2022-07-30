ac2406b0212a4aa0b82b04525fa7de24bd6fe1b9
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-30 10:46:58 -0300

    Exercícios

--- modulo13_react/react_base/src/store/modules/example/reducer.js
+++ modulo13_react/react_base/src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
@@ -5,10 +7,19 @@ const initialState = {
 // eslint-disable-next-line func-names
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso!');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu erro!');
+      return state;
+    }
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição!');
+      return state;
     }
     default: {
       return state;
---
+++ modulo13_react/react_base/src/store/modules/example/sagas.js
@@ -0,0 +1,24 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import * as actions from './actions';
+import * as types from '../types';
+
+const requisicao = () =>
+  // eslint-disable-next-line no-unused-vars
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+
+function* exampleRequest() {
+  try {
+    yield call(requisicao);
+    yield put(actions.clicaBotaoSuccess());
+  } catch {
+    toast.error('Deu erro.');
+    yield put(actions.clicaBotaoFailure());
+  }
+}
+
+export default all([takeLatest(types.BOTAO_CLICADO_REQUEST, exampleRequest)]);
