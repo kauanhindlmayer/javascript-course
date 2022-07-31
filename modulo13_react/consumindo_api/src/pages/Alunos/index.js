b0e9f08126cde8929863da6ec5c93ff5a0354414
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-31 11:09:56 -0300

    Exercícios

--- modulo13_react/consumindo_api/src/pages/Alunos/index.js
+++ modulo13_react/consumindo_api/src/pages/Alunos/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
 export default function Alunos() {
+  const [alunos, setAlunos] = useState();
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      console.log(response.data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <h1>Login</h1>
--- modulo13_react/consumindo_api/src/services/axios.js
+++ modulo13_react/consumindo_api/src/services/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://192.168.15.4',
+  // baseURL: 'http://192.168.15.4',
+  baseURL: 'http://localhost:3001',
 });
