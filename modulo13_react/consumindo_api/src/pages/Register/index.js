df7713ae12fb4a3dac97a58e9536bc7580ccf876
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-07-31 10:04:01 -0300

    Exercícios

--- modulo13_react/consumindo_api/src/pages/Register/index.js
+++ modulo13_react/consumindo_api/src/pages/Register/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Login() {
+export default function Register() {
   return (
     <Container>
       <h1>Login</h1>
